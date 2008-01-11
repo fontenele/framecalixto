12e0a018c96ddf1c48e150e024873ccbe03a7884
Calixto Jorge Gonçalves <calixto.jorge@gmail.com>
2008-01-11 12:35:49 +0000

    Adicionando o barrador de versão.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 /**
 * Arquivo de indice para o funcionamento do sistema
 */
+if(phpversion() < $versao = '5.1.2') throw new Exception(sprintf('O Calixto Framework não funciona com versão inferior a %s.',$versao));
 error_reporting(E_ALL | E_STRICT);
 header("Content-type:text/html; charset=utf-8");
 date_default_timezone_set('America/Sao_Paulo');
