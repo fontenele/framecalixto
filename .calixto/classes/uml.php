98a962ec65dd86632a8077b1ee08e8eebdf915b2
Calixto Jorge Gonçalves <calixto.jorge@gmail.com>
2008-07-22 04:17:38 +0000

    Desistindo do documentador e adotando o Bouml

--- .calixto/classes/uml.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('externas/UML/UML.php');
-/**
-* Classe responsável por gerar o XMI de definição UML
-* @package FrameCalixto
-* @subpackage Utilitários
-*/
-class uml extends php_uml{}
-?>
