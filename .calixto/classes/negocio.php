da9a665c642a0eb302d029147c82ec4c389af66e
Calixto Jorge Gonçalves <calixto.jorge@gmail.com>
2008-07-29 03:44:55 +0000

    Iniciando desvinculação do frame com projetos

--- .calixto/classes/negocio.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* Classe de representação de uma camada de negócio
-* A camada de negócio é a parte que engloba as regras e efetua os comandos de execução de um sistema, esta classe especializa e padroniza a classe de negócio
-* @package FrameCalixto
-* @subpackage Negocio
-*/
-abstract class negocio extends objeto{}
-?>
