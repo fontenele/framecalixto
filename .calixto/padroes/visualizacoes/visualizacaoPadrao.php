daaf06a6c96fd4d8f83fa7fcbe3600c8450a3fd5
Calixto Jorge Gonçalves <calixto.jorge@gmail.com>
2009-12-05 01:23:02 +0000

    Ajuste de carga de template com preferência pelo tema

--- .calixto/padroes/visualizacoes/visualizacaoPadrao.php
+++ .calixto/padroes/visualizacoes/visualizacaoPadrao.php
@@ -30,32 +30,49 @@ class visualizacaoPadrao extends visualizacao{
 	* @param string Nome do arquivo de formatação da visualização
 	*/
 	function mostrar($pagina = null){
+		$template = false;
         switch(true){
             case $pagina:
                 $pagina = $this->pegar($pagina);
+				$template = true;
             break;
             case $this->template:
                 $pagina = $this->pegar($this->template.'.html');
+				$template = true;
             break;
             default:
-				if(is_file($this->template_dir.$this->controle.'.html')){
-	    			$pagina = $this->pegar($this->controle.'.html');
-				}else{
-					if (preg_match('/(.*)(_verEdicao|_verPesquisa)$/', $this->controle, $resultado)) {
-						$this->template_dir = definicaoPasta::tema();
-						if($resultado[2] == '_verEdicao'){
-							if(is_file($this->template_dir.'controlePadrao_verEdicao.html'))
-								$pagina = $this->pegar('controlePadrao_verEdicao.html');
-						}else{
-							if(is_file($this->template_dir.'controlePadrao_verPesquisa.html'))
-								$pagina = $this->pegar('controlePadrao_verPesquisa.html');
+				$arNomeTema = explode('/',definicaoPasta::tema());
+				if(!($nomeTema = array_pop($arNomeTema))){$nomeTema = array_pop($arNomeTema);};
+				switch(true){
+					case (is_file($this->template_dir.$nomeTema.'_'.$this->controle.'.html')):
+		    			$pagina = $this->pegar($nomeTema.'_'.$this->controle.'.html');
+						$template = true;
+					break;
+					case (is_file($this->template_dir.$this->controle.'.html')):
+		    			$pagina = $this->pegar($this->controle.'.html');
+						$template = true;
+					break;
+					default:
+						if (preg_match('/(.*)(_verEdicao|_verPesquisa)$/', $this->controle, $resultado)) {
+							$this->template_dir = definicaoPasta::tema();
+							if($resultado[2] == '_verEdicao'){
+								if(is_file($this->template_dir.'controlePadrao_verEdicao.html')){
+									$pagina = $this->pegar('controlePadrao_verEdicao.html');
+									$template = true;
+								}
+							}else{
+								if(is_file($this->template_dir.'controlePadrao_verPesquisa.html')){
+									$pagina = $this->pegar('controlePadrao_verPesquisa.html');
+									$template = true;
+								}
+							}
 						}
-					}
-					if(!$pagina){
-						throw new erroInclusao("Template não encontrado! ./{$this->template_dir}{$this->controle}.html");
-					}
 				}
+			break;
         }
+		if(!$template){
+			throw new erroInclusao("Template não encontrado! ./{$this->template_dir}{$this->controle}.html");
+		}
         if(controle::requisicaoAjax()) {
             echo $pagina;
             return;
