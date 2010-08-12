aea373d8de759d6bc6c1562a2a924526e198a20f
guilherme@syntaxbr.com <guilherme@syntaxbr.com@307d3402-3428-0410-a6a9-f3189685af3e>
2010-08-12 14:08:11 +0000

    voltando js para voltar a funcionar no firefox, porem continua com bug no internet explorer

--- trunk/novoProjeto/utilitario/scripts/CUtilitario_geradorDefinirEntidade.js
+++ trunk/novoProjeto/utilitario/scripts/CUtilitario_geradorDefinirEntidade.js
@@ -95,8 +95,12 @@ gerador = {
 		}
 	},
 	passarNome:function (obj){
-		$('.ln_' + this.pegarLinha(obj) + ' td.propriedade').html(obj.val());
-		$('.ln_' + this.pegarLinha(obj) + ' input.propriedade').val(obj.val());
+		/**
+		 * TODO
+		 * $('.ln_' + this.pegarLinha(obj) + ' td.propriedade').html(obj.val());
+		 * $('.ln_' + this.pegarLinha(obj) + ' input.propriedade').val(obj.val());
+		 */
+		$('.ln_' + this.pegarLinha(obj) + ' .propriedade').val(obj.val());
 	},
 	remover:function(linha){
 		$('.ln_' + linha).remove();
