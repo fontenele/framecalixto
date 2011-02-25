68f156ccea9b85a6e96b5e87eef59dad7fcdb5a2
calixto.jorge@gmail.com <calixto.jorge@gmail.com@307d3402-3428-0410-a6a9-f3189685af3e>
2011-02-25 21:45:03 +0000

    Modificando cadastro de menu

---
+++ trunk/novoProjeto/menu/classes/CMenu_verEdicaoArvore.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Classe de controle
+* Executa a gravação de um objeto : Menu
+* @package Sistema
+* @subpackage Menu
+*/
+class CMenu_verEdicaoArvore extends controlePadraoVerEdicao{
+	public function inicial(){
+		parent::inicial();
+	}
+}
+?>
--- trunk/novoProjeto/menu/classes/NMenu.php
+++ trunk/novoProjeto/menu/classes/NMenu.php
@@ -5,7 +5,7 @@
 * @package Sistema
 * @subpackage Menu
 */
-class NMenu extends negocioPadrao{
+class NMenu extends negocioPadraoArvore{
 	/**
 	* @gerador variavelPadrao
 	* @var integer Código
@@ -22,10 +22,37 @@ class NMenu extends negocioPadrao{
 	*/
 	public $txDescricao;
 	/**
+	* @gerador variavelPadrao
+	* @var string Menu
+	*/
+	public $menu;
+	/**
+	* @gerador variavelPadrao
+	* @var integer idE
+	*/
+	public $idE;
+	/**
+	* @gerador variavelPadrao
+	* @var integer idD
+	*/
+	public $idD;
+	/**
 	* Retorna o nome da propriedade que contém o valor chave de negócio
 	* @gerador metodoPadrao
 	* @return string
 	*/
 	function nomeChave(){ return 'idMenu'; }
+	/**
+	 * Método que define o nome da chave esquerda da arvore
+	 */
+	public function nomeChaveEsquerda(){
+		return 'idE';
+	}
+	/**
+	 * Método que define o nome da chave direita da arvore
+	 */
+	public function nomeChaveDireita(){
+		return 'idD';
+	}
 }
 ?>
