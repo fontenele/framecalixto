f9503baeb6ba500083a74c6769d4a92023cdab9e
guilherme@syntaxbr.com <guilherme@syntaxbr.com@307d3402-3428-0410-a6a9-f3189685af3e>
2011-05-11 22:08:12 +0000

    TCPDF implementada

--- trunk/novoProjeto/.sistema/classes/Spdf.php
+++ trunk/novoProjeto/.sistema/classes/Spdf.php
@@ -5,12 +5,21 @@
 * @subpackage visualização
 */
 class Spdf extends pdf{
+	public function configurar(){
+		$this->SetHeaderMargin(PDF_MARGIN_HEADER);
+		$this->SetFooterMargin(PDF_MARGIN_FOOTER);
+		$this->SetSubject('FrameCalixto');
+		$this->SetKeywords('framecalixto, framework, php, php5, sistemas');
+	}
 	/**
 	* Método de montagem do cabeçalho do pdf
 	*/
 	function cabecalho(){
-		//$this->Image('logo_pb.png',10,8,33);
-		$this->SetFont('Arial','B',12);
+		$this->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING);
+		$this->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+		$this->Image(realpath('.sistema/imagens/framecalixto.png'),9,1,19);
+		$this->SetFont('Times','B',12);
+		$this->Ln(2);
 		$this->Cell(0,10,'Modifique o título do cabeçalho em Spdf::cabecalho();',1,0,'C');
 		$this->Ln(20);
 		$this->SetFont('Times','B',8);
@@ -19,8 +28,9 @@ class Spdf extends pdf{
 	* Método de montagem do rodapé do pdf
 	*/
 	function rodape(){
+		$this->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 		$this->SetY(-15);
-		$this->SetFont('Arial','I',8);
+		$this->SetFont('Times','I',8);
 		$this->Cell(0,10,'Página '.$this->PageNo().'/{nb}',0,0,'C');
 	}
 }
