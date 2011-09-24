e6a3d389d1691e3fbe7deb2c1f301d236967f619
calixto.jorge <calixto.jorge@307d3402-3428-0410-a6a9-f3189685af3e>
2011-09-24 09:33:26 +0000

    removendo opção errada no erro de negócio

--- trunk/.calixto/tiposDeErros/erroNegocio.php
+++ trunk/.calixto/tiposDeErros/erroNegocio.php
@@ -12,9 +12,7 @@ class erroNegocio extends erro{
 		$st = ($this->message)?"Causa [$this->message]":'';
 		return "Ocorreu um erro de Regra de negócio! \n
 		Na linha [{$this->line}] do arquivo [{$this->file}]\n
-		{$st}\n
-		Comando causador:\n
-		{$this->comando}";
+		{$st}\n";
 	}
 }
 ?>
