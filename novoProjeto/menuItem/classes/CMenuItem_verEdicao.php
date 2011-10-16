f3ba7a98de9759f69b886a3f51b66c0e6a06951c
Calixto Jorge Gonçalves <calixto.jorge@gmail.com>
2011-10-16 04:40:04 +0000

    Removendo menuItem

--- novoProjeto/menuItem/classes/CMenuItem_verEdicao.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* Classe de controle
-* Cria a visualização de um objeto : Menu Item
-* @package Sistema
-* @subpackage Menu Item
-*/
-class CMenuItem_verEdicao extends controlePadraoVerEdicao{}
-?>
--- novoProjeto/menuItem/classes/CMenuItem_verPesquisa.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* Classe de controle
-* Cria a visualização da pesquisa de um objeto : Menu Item
-* @package Sistema
-* @subpackage Menu Item
-*/
-class CMenuItem_verPesquisa extends controlePadraoPesquisa{}
-?>
