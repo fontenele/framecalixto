f6950e77f299101da43cd0032fa6412c363c15c8
Calixto Jorge Gonçalves <calixto.jorge@gmail.com>
2011-10-27 10:24:01 +0000

    corrigindo testador de conexao

--- novoProjeto/utilitario/classes/CUtilitario_testarConexao.php
+++ novoProjeto/utilitario/classes/CUtilitario_testarConexao.php
@@ -12,7 +12,7 @@ class CUtilitario_testarConexao extends controlePadrao{
 			$tipo = isset($_REQUEST['banco']['tipo'][0])?$_REQUEST['banco']['tipo'][0]:null;
 			$servidor = isset($_REQUEST['banco']['servidor'][0])?$_REQUEST['banco']['servidor'][0]:null;
 			$porta = isset($_REQUEST['banco']['porta'][0])?$_REQUEST['banco']['porta'][0]:null;
-			$banco = isset($_REQUEST['banco']['banco'][0])?$_REQUEST['banco']['banco'][0]:null;
+			$banco = isset($_REQUEST['banco']['nome'][0])?$_REQUEST['banco']['nome'][0]:null;
 			$usuario = isset($_REQUEST['banco']['usuario'][0])?$_REQUEST['banco']['usuario'][0]:null;
 			$senha = isset($_REQUEST['banco']['senha'][0])?$_REQUEST['banco']['senha'][0]:null;
 			try{
